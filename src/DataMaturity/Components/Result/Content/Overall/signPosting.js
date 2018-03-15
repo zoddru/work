e39bdeca4d9d15a2a666df2716d1d989b45d5978
dru zod <mattdalt10@gmail.com>
2018-03-15 00:46:02 +0000

    signPosting styles

--- src/DataMaturity/Components/Result/Content/Overall/signPosting.js
+++ src/DataMaturity/Components/Result/Content/Overall/signPosting.js
@@ -1,7 +1,2 @@
 import React from 'react';
-export default <div className="signPosting">
-    <ul>
-        <li><a href="xxx">XXX</a></li>
-        <li><a href="xxx">XXX</a></li>
-    </ul>
-</div>;
+export default null;
