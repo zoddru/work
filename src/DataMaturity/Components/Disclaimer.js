56f74f8fbb5e8f641533a4b5531a965580791b17
Matt Dalton <matthew.dalton@porism.com>
2018-03-26 15:35:40 +0100

    minor change to the disclaimer

--- src/DataMaturity/Components/Disclaimer.js
+++ src/DataMaturity/Components/Disclaimer.js
@@ -18,7 +18,7 @@ export default class Disclaimer extends React.Component {
                         This prototype has not been independently verified. The LGA and Nesta, their respective directors, employees or affiliates do not make any representation or warranty, express or implied, as to the accuracy, completeness or fairness of any information provided in connection with the prototype, and none of such parties shall have any liability for the information contained in, or for any omissions from the prototype.
                     </p>
                     <p>
-                        This prototype references work by HESA - for more information visit:
+                        This prototype references work by HESA - for more information visit:&nbsp;
                         <a className="link" href="https://www.hesa.ac.uk/support/tools/data-capability/full/assess-maturity">
                             https://www.hesa.ac.uk/support/tools/data-capability/full/assess-maturity</a>
                     </p>
