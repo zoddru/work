d4ed35096d5541c0546ed1bacdf3fd41d6bc333f
Matt Dalton <matthew.dalton@porism.com>
2018-03-28 17:24:33 +0100

    sign out options in drop down menu

--- src/DataMaturity/Components/TopBar.js
+++ src/DataMaturity/Components/TopBar.js
@@ -1,4 +1,70 @@
 import React from 'react';
+const Fragment = React.Fragment;
+
+const dropdownInDelay = 200;
+const dropdownOutDelay = 200;
+const links = {
+    subscriptionLink: 'http://about.esd.org.uk/subscription-benefits',
+    changePasswordLink: 'https://signin.esd.org.uk/change.html?app=esd',
+    myProfileLink: 'https://signin.esd.org.uk/profile.html?app=esd'
+};
+
+class Dropdown extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.timeoutId = null;
+        this.state = { isOpen: false, inDom: false };
+    }
+
+    addToDom() {
+        this.setState(prevState => ({ inDom: true }));
+    }
+
+    removeFromDom() {
+        this.setState(prevState => ({ inDom: false }));
+    }
+
+    open() {
+        this.setState(prevState => ({ isOpen: true }));
+
+        clearTimeout(this.timeoutId);
+        if (this.state.inDom)
+            return;
+        this.timeoutId = setTimeout(this.addToDom.bind(this), dropdownInDelay);
+    }
+
+    close() {
+        this.setState(prevState => ({ isOpen: false }));
+
+        clearTimeout(this.timeoutId);
+        if (!this.state.inDom)
+            return;
+        this.timeoutId = setTimeout(this.removeFromDom.bind(this), dropdownOutDelay);
+    }
+
+    mouseEnter() {
+        this.open();
+        
+    }
+
+    mouseLeave() {
+        this.close();
+    }
+
+    render() {
+        const { label, children } = this.props;
+        const { isOpen, inDom } = this.state;
+
+        return <div className={`dropdown ${isOpen ? 'open' : ''}`} onMouseEnter={this.mouseEnter.bind(this)} onMouseLeave={this.mouseLeave.bind(this)}>
+            <a>{label}</a>
+            {inDom && <div className="content">
+                {children}
+            </div>}
+        </div>;
+    }
+}
+
 
 function getCurrentUrl() {
     if (typeof window === 'undefined' || !window.location)
@@ -16,20 +82,27 @@ function WhenSignedIn(props) {
     const user = status.user;
     const organisation = user.organisation;
 
+    const organisationEl = !!organisation && <span className="label subtle">{organisation.label}</span>;
+    const subscriptionEl = !!organisation
+        ? <a href={links.subscriptionLink}>
+            Organisation is {!organisation.isSubscribed ? "NOT" : ""} subscribed to LG Inform Plus
+        </a> : null;
+
     return <div className="credentials">
         <HelpLink />
-        <div className="dropdown">
-            <a>{user.label}</a>
-            {/* <div className="content">
-                <p>
-                    {user.label}
-                </p>
-            </div> */}
-        </div>
-    </div>;
-
-    return <div className="signInDetails">
-        You are signed in as <strong>{user.label}</strong>{!!organisation ? ` from ${organisation.label}` : ''}. <a className="button" href={"/signout?returnUrl=" + encodedUrl}>sign out</a>
+        <Dropdown label={user.label}>
+            <span className="label">
+                {user.label}
+            </span>
+            <span className="label subtle">
+                {user.email}
+            </span>
+            {organisationEl}
+            {subscriptionEl}
+            <a href={links.changePasswordLink}>Change password</a>
+            <a href={links.myProfileLink}>My profile</a>
+            <a className="signOut" href={`/signout?returnUrl=${encodedUrl}`}>Sign out</a>
+        </Dropdown>
     </div>;
 }
 
