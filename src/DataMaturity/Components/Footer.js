7919747e47a376ece8ecb7166fe565ee59ab4c4b
Matt Dalton <matthew.dalton@porism.com>
2018-04-11 16:18:42 +0100

    version

--- src/DataMaturity/Components/Footer.js
+++ src/DataMaturity/Components/Footer.js
@@ -13,14 +13,14 @@ export default class Footer extends React.Component {
         return <div class="columns">
             <div class="column">
                 <a href="/disclaimer" onClick={(e) => this.setHistory(e, '/disclaimer')}>Disclaimer</a>
-                <span className="version">                    
-                    {common.version}
-                </span>
             </div>
             <div class="column">
                 <a href="https://www.local.gov.uk/">LGA</a>
                 <a href="https://www.nesta.org.uk/">Nesta</a>
                 <a href="https://porism.com/">Porism</a>
+                <span className="version">                    
+                    {common.version}
+                </span>
                 <span>
                     Copyright © {new Date().getFullYear()}
                 </span>
