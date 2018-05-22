445db73dc3582013a8ddaf5db60a7c8a3883d1ef
Matt Dalton <matthew.dalton@porism.com>
2018-05-22 16:55:38 +0100

    skills case study change

--- src/DataMaturity/Components/Result/Content/Skills/caseStudy.js
+++ src/DataMaturity/Components/Result/Content/Skills/caseStudy.js
@@ -2,10 +2,12 @@ import React from 'react';
 export default <div class="caseStudy">
     <p><strong>Essex’s Data Analytics Hub</strong></p>
     <p>
-        Essex has developed a data analytics hub through which it has grown its own in-house capacity. The data analysts are responsible for bringing together data from different parts of the council, integrating and using for a range of data-informed projects.
+        Essex Partners have developed the Essex Data programme to tackle issues earlier through smarter use of data.  The programme provides the capability and capacity to safely share and match structured data about families, households and the local area from across different organisations to help us predict future areas of vulnerability. The platform enables Essex to safely share pseudonymised data at source and merge this at a household level with data from different organisations.  The platform has allowed analysts from different organisations to create predictive risk models, using risk stratification and multiple linear regression, build algorithms and show correlations between different variables and datasets in a quick and efficient way.  
     </p>
     <p>
-        The covers a wide range of data skills, from management through to analytical techniques. One example is the use of predictive algorithms, which which help the council to identify potential adverse events before they happen. For instance, the council has recently undertaken work to predict which areas are most likely to have children which are not school ready at age 4. This enables them to intervene early with additional support for children in those areas. Alongside the sophisticated technical skills of the team, the staff who use the insights produced by the hub are required to have a high level of data literacy to enable them to use them in decision making.
+        Essex has supported its staff to become technically proficient in these techniques by running a series of masterclasses aligned to real life examples lead by trained experts in different techniques. They have used data accelerator courses and agile methods to develop problem solving ability, spreading skills and expertise to others using PODs and POD masters, breaking down silo and startup methods of working.
     </p>
-    {/* [awaiting additional information from Essex] */}
+    <p>
+        Staff in the Data Partnership can create dashboards to shows hot spots that can be published to different organisations.  The platform has shown the importance of starting well by scoping and helping customers to ask the right types questions first.  The experience of running the platform has shown that people need help to clearly specify the problem to be solved, and that success of predictive analytics / data sharing projects are as much about culture of an organisation as it is about the people and skills needed to do the work. It’s good to start small, with a certain topic or pilot and take people on the journey, to help an organisation transform its culture.
+    </p>
 </div>;
